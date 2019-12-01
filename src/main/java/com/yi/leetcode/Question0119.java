61b9857f6915b51f93c23e6e45278255bcf43bd7
wjy <1875256338@qq.com>
2019-12-01 19:24:00 +0800

    2019/11/07 杨辉三角II

---
+++ src/main/java/com/yi/leetcode/Question0119.java
@@ -0,0 +1,40 @@
+package com.yi.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Question0119 {
+
+    public static List<Integer> getRows(int rowIndex) {
+        List<Integer> list = new ArrayList<>();
+        Integer cur = 1;
+        for (int i = 0; i <= rowIndex; i++) {
+            list.add(cur);
+            cur = cur * (rowIndex - i) / (i + 1);
+        }
+        return list;
+    }
+
+    public static List<Integer> getRows2(int rowNumber) {
+        if (rowNumber < 0) {
+            return new ArrayList<Integer>();
+        }
+        Integer[] list = new Integer[rowNumber + 1];
+        Arrays.fill(list, 1);
+
+        for (int i = 0; i <= rowNumber; i++) {
+            for (int j = i - 1; j >= 1; j--) {
+                list[j] = list[j] + list[j - 1];
+            }
+        }
+
+        return Arrays.asList(list);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getRows(5).toString());
+        System.out.println(getRows2(6).toString());
+    }
+
+}
