b715a0e53e822cf0b158d1370c0f0b2a81da2c3d
wjy <1875256338@qq.com>
2019-12-03 23:09:50 +0800

    2019/12/03 leetcode 50 求 x^n 的问题

---
+++ src/main/java/com/yi/leetcode/Question0050.java
@@ -0,0 +1,33 @@
+package com.yi.leetcode;
+
+public class Question0050 {
+
+    public static double fastPow(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        double half = fastPow(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+
+    public static double myPow(double x, int n) {
+        if (n < 0) {
+            n = -n;
+            x = 1 / x;
+        }
+
+        return fastPow(x, n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myPow(2, 10));
+        System.out.println(myPow(2, -10));
+    }
+
+}
